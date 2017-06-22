d60c2c28512c11d163f1bfef102ebe02a2fe3a3c
Kyle Martin <kmart120@hotmail.com>
2017-06-22 15:41:07 -0400

    modified run_tests.bash to actually run_tests

--- run_tests.bash
+++ run_tests.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+declare -a thres=(300, 3600, 7200)
+declare -a num=(25, 100)
+
 for filename in *.txt; do
     num_lines=$(wc -l < $filename)
-    if [[ $num_lines -gt 50 ]]; then
-       python getFramerate.py
+    if [[ $num_lines -gt 99 ]]; then
+	for (( i=0 ; i<3 ; i++ )); do
+	    for (( j=0 ; j<2 ; j++ )); do
+		dir="${filename}__${thres[$i]}_${num[$j]}"
+	    	python getFramerate.py -t ${thres[$i]} -r 1000000 -d $dir\
+		       -p password -f $filename -n ${num[$j]}
+	    done
+	done
     fi
 done
