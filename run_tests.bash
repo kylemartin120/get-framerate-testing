7cc1d470bbf3f8cdc86d84fb2df81117d077303d
Kyle Martin <kmart120@hotmail.com>
2017-06-27 11:16:32 -0400

    changed the code to use one threshold and num value, but run twice for each test

--- run_tests.bash
+++ run_tests.bash
@@ -6,18 +6,16 @@ else
     password="\"\""
 fi
 
-declare -a thres=(300 3600 7200)
-declare -a num=(25 100)
+THRES=5400
+NUM=25
 
 for filename in *.txt; do
     num_lines=$(wc -l < $filename)
     if [[ $num_lines -gt 99 ]]; then
-	for (( i=0 ; i<3 ; i++ )); do
-	    for (( j=0 ; j<2 ; j++ )); do
-		dir="${filename}__${thres[$i]}_${num[$j]}"
-	    	python getFramerate.py -t ${thres[$i]} -r 1000000 -d $dir\
-		       -p $password -f $filename -n ${num[$j]}
-	    done
+	for (( j=0 ; j<2 ; j++ )); do
+	    dir="${filename}__${THRES}_${NUM}_${j}"
+	    python getFramerate.py -t $THRES -r 1000000 -d $dir\
+		   -p $password -f $filename -n $NUM
 	done
     fi
 done
